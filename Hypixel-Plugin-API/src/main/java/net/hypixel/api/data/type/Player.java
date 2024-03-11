20e4f7cc307855dae49788a92b19426c994be889
Pryrobite <143216072+Pryrobite@users.noreply.github.com>
2024-03-11 08:46:09 -0500

    Update Player.java

--- Hypixel-Plugin-API/src/main/java/net/hypixel/api/data/type/Player.java
+++ Hypixel-Plugin-API/src/main/java/net/hypixel/api/data/type/Player.java
@@ -1,3 +1,22 @@
 package net.hypixel.api.data.type;
 
+import net.hypixel.api.data.type.GameType;
+import org.bukkit.event.Player;
+
+public class Player extends GameType {
+  public Player(Class<T> player, GameType gameType) {
+    if(player.isOnline() && gameType == GameType.BEDWARS) {
+      player.getAllowedFlight(false);
+         player.connection.getConnection();
+      /** 
+      * @Hypixel {link}
+      *
+      *  Minecraft.getInstance();
+      *  Enable Mixins in Project Module Settings @Hypixel
+   */
+    }
+    super(player);
+  }
+}
+
 
